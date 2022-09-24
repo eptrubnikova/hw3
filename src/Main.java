e01f5539248a5841bd04a37f06f39a02743902e0
eptrubnikova <eptrubnikova@gmail.com>
2022-09-24 23:51:48 +0600

    начальный коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
 public class Main {
     public static void main(String[] args) {
+System.out.println("Задача 1");
+        int a = 5;
+        System.out.println("Значение переменной a с типом int равно " + a);
+        byte b = 10;
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        short c = 15;
+        System.out.println("Значение переменной c с типом short равно " + c);
+        long d = 20;
+        System.out.println("Значение переменной d с типом long равно " + d);
+        float e = 30f;
+        System.out.println("Значение переменной e с типом float равно " + e);
+        double f = 40;
+        System.out.println("Значение переменной f с типом double равно " + f);
 
-        ;
+        System.out.println("Задача 2");
+        double j = 27.12;
+        System.out.println("Значение переменной h с типом double равно " + j);
+        long k = 987678965549L;
+        System.out.println("Значение переменной h с типом long равно " + k);
+        double l = 2.782;
+        System.out.println("Значение переменной h с типом double равно " + l);
+        boolean m = false;
+        System.out.println("Значение переменной h с типом boolean равно " + m);
+        byte g = 67;
+            System.out.println("Значение переменной g с типом byte равно " + g);
+        short h = 569;
+        System.out.println("Значение переменной h с типом short равно " + h);
+        int i = -159;
+        System.out.println("Значение переменной h с типом int равно " + i);
+        short o = 27897;
+        System.out.println("Значение переменной h с типом short равно " + o);
+
+
+
+
     }
 }
